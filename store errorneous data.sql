b0d36738631cd0cc5b1581f72abea97261305208
mayanksinghal <mayank.singhal@nagarro.com>
2023-01-19 17:51:23 +0530

    copy options for errorneous data

---
+++ store errorneous data.sql
@@ -0,0 +1,84 @@
+CREATE OR REPLACE DATABASE COPY_DB;
+
+
+CREATE OR REPLACE TABLE  COPY_DB.PUBLIC.ORDERS (
+    ORDER_ID VARCHAR(30),
+    AMOUNT VARCHAR(30),
+    PROFIT INT,
+    QUANTITY INT,
+    CATEGORY VARCHAR(10),
+    SUBCATEGORY VARCHAR(30));
+
+--Prepare stage object--
+create or replace stage copy_db.public.aws_stage_copy
+    url ='s3://snowflakebucket-copyoption/returnfailed/';
+    
+list @copy_db.public.aws_stage_copy;
+
+-- validate first n rows --
+copy into copy_db.public.orders
+    from @copy_db.public.aws_stage_copy
+    file_format = (type=csv field_delimiter=',' skip_header=1)
+    pattern='.*Order.*'
+    validation_mode=return_1_rows; //here first row is errorneous || copy doesn't happen
+
+
+-- show all errorneous data --
+copy into copy_db.public.orders
+    from @copy_db.public.aws_stage_copy
+    file_format = (type=csv field_delimiter=',' skip_header=1)
+    pattern='.*Order.*'
+    validation_mode=return_errors;
+
+-------------- Working with error results -----------
+
+---- 1) Saving rejected files after VALIDATION_MODE ---- 
+
+// Storing rejected /failed results in a table
+CREATE OR REPLACE TABLE rejected AS 
+select rejected_record from table(result_scan(last_query_id()));
+
+SELECT * FROM rejected;
+    
+
+---- 2) Saving rejected files without VALIDATION_MODE ---- 
+
+COPY INTO COPY_DB.PUBLIC.ORDERS
+    FROM @aws_stage_copy
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    pattern='.*Order.*'
+    ON_ERROR=CONTINUE;
+    
+select * from table(validate(orders, job_id => '_last'));
+
+
+---- 3) Working with rejected records ---- 
+
+SELECT REJECTED_RECORD FROM rejected;
+
+CREATE OR REPLACE TABLE rejected_values as
+SELECT 
+SPLIT_PART(rejected_record,',',1) as ORDER_ID, 
+SPLIT_PART(rejected_record,',',2) as AMOUNT, 
+SPLIT_PART(rejected_record,',',3) as PROFIT, 
+SPLIT_PART(rejected_record,',',4) as QUATNTITY, 
+SPLIT_PART(rejected_record,',',5) as CATEGORY, 
+SPLIT_PART(rejected_record,',',6) as SUBCATEGORY
+FROM rejected; 
+
+SELECT * FROM rejected_values;
+
+truncate table orders;
+
+-----truncatecolumns:Boolean that specifies whether totruncate text strings that exceed the target column length-------
+
+COPY INTO COPY_DB.PUBLIC.ORDERS
+    FROM @aws_stage_copy
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    pattern='.*Order.*'
+    on_error = continue
+    truncatecolumns = true;
+    
+select * from orders;
+
+drop table orders;
